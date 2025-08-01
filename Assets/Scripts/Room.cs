94527a2fb3a3bba9b868acbfc508626aa7af4e54
RiffStudios <68124263+RiffStudios@users.noreply.github.com>
2025-07-31 22:01:30 -0700

    Adding room class and moving things around

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -6,11 +6,36 @@ public class Room
     public BoundsInt bounds;
     public Vector2Int center;
     public HashSet<Vector2Int> floorPositions;
+    public HashSet<Vector2Int> innerTiles;
 
     public Room(BoundsInt bounds, HashSet<Vector2Int> floorPositions)
     {
         this.bounds = bounds;
         center = Vector2Int.RoundToInt(bounds.center);
         this.floorPositions = floorPositions;
+
+        CalculateInnerTiles();
+    }
+
+    private void CalculateInnerTiles()
+    {
+        innerTiles = new HashSet<Vector2Int>();
+        foreach (var position in floorPositions)
+        {
+            bool isInnerTile = true;
+            foreach (var direction in ProceduralGeneration.Direction2D.CardinalDirectionsList)
+            {
+                if(!floorPositions.Contains(position + direction)) 
+                {
+                    isInnerTile = false;
+                    break;
+                }
+            }
+
+            if (isInnerTile)
+            {
+                innerTiles.Add(position);
+            }
+        }
     }
 }
