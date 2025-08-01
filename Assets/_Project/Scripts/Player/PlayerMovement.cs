43c0c29a2c062233f48084fa6a1330736a787b0f
AshleyWills <41488426+AshleyWillsDev@users.noreply.github.com>
2025-08-01 15:16:35 +0100

    Drawing on screen prototype
    Left clicking and dragging will allow you to draw on the screen. When letting go of left click a raycast is created from the centroid of all the point of the drawing.
    
    When drawing player movement is stopped and started again when drawing has stopped

--- Assets/_Project/Scripts/Player/PlayerMovement.cs
+++ Assets/_Project/Scripts/Player/PlayerMovement.cs
@@ -8,6 +8,9 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody _rigidbody;
     private Vector2 _moveInputValue;
     private Vector3 _finalMovement;
+
+    private bool _canMove = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -28,14 +31,46 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
-        Vector2 dir = _moveInputValue.normalized;
-        //myAnim.SetFloat("Speed", magnitude);
-        _finalMovement = new Vector3(dir.x, 0, dir.y) * speed;
-        //_rigidbody.linearVelocity = new Vector3(dir.x * speed * Time.deltaTime,_rigidbody.linearVelocity.y, dir.y * speed * Time.deltaTime);
+        if (_canMove == true)
+        {
+            Vector2 dir = _moveInputValue.normalized;
+            //myAnim.SetFloat("Speed", magnitude);
+            _finalMovement = new Vector3(dir.x, 0, dir.y) * speed;
+            //_rigidbody.linearVelocity = new Vector3(dir.x * speed * Time.deltaTime,_rigidbody.linearVelocity.y, dir.y * speed * Time.deltaTime);
+        }
+        else
+        {
+            _finalMovement = new Vector3(0, 0, 0);
+        }
+    }
+
+    private void CanMove(bool onDraw)
+    {
+        if (onDraw == true)
+        {
+            _canMove = false;
+        }
+        else
+        {
+            _canMove = true;
+        }
     }
     
+    private void OnEnable()
+    {
+        //event for when drawing on screen
+        DrawOnScreen.onDraw += CanMove;
+
+    }
+
+    private void OnDisable()
+    {
+        DrawOnScreen.onDraw -= CanMove;
+    }
+        
+
     #region Input
-    
+
     public void OnMove(InputValue value)
     {
         _moveInputValue = value.Get<Vector2>();
---
+++ Assets/_Project/Scripts/Prototype/DrawOnScreen.cs
@@ -0,0 +1,98 @@
+using System;
+using UnityEditor;
+using UnityEditor.ShaderGraph;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class DrawOnScreen : MonoBehaviour
+{
+    private LineRenderer line;
+    private Vector3 previousPosition;
+    private Vector3 currentPosition;
+    private Vector3 mousePosition;
+    private Vector3[] linePositions;
+    public Camera mainCamera;
+    public static event Action<bool> onDraw;
+    [SerializeField] private float raycastAngleY = -50;
+    [SerializeField] private float minimumLineDrawingDistance = 0.001f;
+
+
+
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.positionCount = 1;
+        previousPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPosition = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5f));
+
+        if (Input.GetMouseButton(0))
+        {
+            onDraw?.Invoke(true);
+
+            Draw();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            onDraw?.Invoke(false);
+
+            CalculateDrawnCentroid();
+
+            line.positionCount = 0;
+        }
+
+    }
+
+    private void Draw()
+    {
+        if (previousPosition == transform.position)
+        {
+            line.SetPosition(0, currentPosition);
+        }
+
+        if (Vector3.Distance(currentPosition, previousPosition) > minimumLineDrawingDistance)
+        {
+            line.positionCount++;
+            line.SetPosition(line.positionCount - 1, currentPosition);
+            previousPosition = currentPosition;
+        }
+    }
+
+    private void CalculateDrawnCentroid()
+    {
+        linePositions = new Vector3[line.positionCount];
+        line.GetPositions(linePositions);
+
+        int arrayLength = linePositions.Length;
+
+        Vector3 totalVectorAmount = new Vector3();
+        Vector3 centroid;
+
+        for (int i = 0; i < arrayLength; i++)
+        {
+            Vector3 position = linePositions[i];
+
+            totalVectorAmount = totalVectorAmount + position;
+        }
+
+        centroid = totalVectorAmount / arrayLength;
+
+        CreateRaycastHit(centroid);
+    }
+
+    private void CreateRaycastHit(Vector3 centroid)
+    {
+        RaycastHit hit;
+
+        Vector3 rayDirection = new Vector3(0, raycastAngleY, 100);
+
+        Physics.Raycast(centroid, rayDirection, 100f);
+        Debug.DrawRay(centroid, rayDirection, Color.red, 5f);
+    }
+
+}
