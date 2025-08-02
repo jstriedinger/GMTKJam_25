2ae83406af3db7b1af6d7b58c02461f59c7b78e1
AshleyWills <41488426+AshleyWillsDev@users.noreply.github.com>
2025-08-03 00:18:17 +0100

    Added Taken Damage Event

--- Assets/_Project/Scripts/Common/Health.cs
+++ Assets/_Project/Scripts/Common/Health.cs
@@ -1,12 +1,16 @@
+using System;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+
+    public static event Action<GameObject> takenDamageEvent;
     [SerializeField] public int totalHealth;
 
     public void TakeDamage(int damage)
     {
         totalHealth = Mathf.Max(0, totalHealth - damage);
+        takenDamageEvent?.Invoke(gameObject);
     }
 
     public bool IsDeath()
