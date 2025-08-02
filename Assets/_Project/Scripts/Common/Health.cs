0085c2640206123edfdb576a29f1e2a7b49c9315
Quincy van der Sluis <theqpowerrr@gmail.com>
2025-08-03 01:13:37 +0200

    WIP Healthbar

--- Assets/_Project/Scripts/Common/Health.cs
+++ Assets/_Project/Scripts/Common/Health.cs
@@ -3,10 +3,19 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] public int totalHealth;
+    [SerializeField] public SpriteRenderer first;
+    [SerializeField] public SpriteRenderer second;
+    [SerializeField] public SpriteRenderer third;
+    [SerializeField] public SpriteRenderer fullheart;
+    [SerializeField] public SpriteRenderer noheart;
 
     public void TakeDamage(int damage)
     {
         totalHealth = Mathf.Max(0, totalHealth - damage);
+        if (totalHealth == 2)
+        {
+
+        }
     }
 
     public bool IsDeath()
