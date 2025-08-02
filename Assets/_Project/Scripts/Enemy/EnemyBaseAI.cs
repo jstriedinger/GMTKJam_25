2bdc4c424fc5d7f66820cb217633cd4f965f1d64
RiffStudios <68124263+RiffStudios@users.noreply.github.com>
2025-08-01 23:53:42 -0700

    Refactoring enemy AI

---
+++ Assets/_Project/Scripts/Enemy/EnemyBaseAI.cs
@@ -0,0 +1,96 @@
+using System;
+using UnityEngine;
+
+public class EnemyBaseAI : MonoBehaviour
+{
+    public enum EnemyAIBehavior { Idle, Patrol, Chase, Retreat, Shoot, Attack }
+
+    protected EnemyAIBehavior state = EnemyAIBehavior.Idle;
+    [HideInInspector] public GameObject player = null;
+    public float speed = 5;
+    public float attackDistance = 1;
+    public float disengageDistance = 1.3f;
+    public float attackCooldown = 0;
+    public int damage = 1;
+
+    // Update is called once per frame
+    virtual protected void Update()
+    {
+        UpdateCooldowns();
+
+        switch (state)
+        {
+            case EnemyAIBehavior.Idle:
+                IdleBehavior();
+                break;
+            case EnemyAIBehavior.Chase:
+                ChaseBehavior();
+                break;
+            case EnemyAIBehavior.Attack:
+                AttackBehavior();
+                break;
+        }
+    }
+
+    protected void UpdateCooldowns()
+    {
+        attackCooldown -= Time.deltaTime;
+    }
+
+    virtual protected void SwitchState(EnemyAIBehavior newState)
+    {
+        state = newState;
+    }
+
+    protected virtual void IdleBehavior()
+    {
+        if (player != null)
+        {
+            SwitchState(EnemyAIBehavior.Chase);
+        }
+    }
+
+    protected virtual void ChaseBehavior()
+    {
+        if (player == null)
+        {
+            SwitchState(EnemyAIBehavior.Idle);
+            return;
+        }
+
+        Vector3 playerDirection = (player.transform.position - transform.position).normalized;
+        transform.position += new Vector3(playerDirection.x, 0, playerDirection.z) * speed * Time.deltaTime;
+
+        if (Vector3.Distance(player.transform.position, transform.position) <= attackDistance)
+        {
+            SwitchState(EnemyAIBehavior.Attack);
+        }
+    }
+
+    protected virtual void AttackBehavior()
+    {
+        if (player == null)
+        {
+            SwitchState(EnemyAIBehavior.Idle);
+            return;
+        }
+
+        if (Vector3.Distance(player.transform.position, transform.position) > disengageDistance)
+        {
+            SwitchState(EnemyAIBehavior.Chase);
+        }
+        else
+        {
+            if (attackCooldown <= 0)
+            {
+                Health health = player.GetComponent<Health>();
+                if (health != null)
+                {
+                    health.TakeDamage(damage);
+                }
+
+                attackCooldown = 2f;
+            }
+        }
+    }
+}
--- Assets/_Project/Scripts/Enemy/EnemyChaseAI.cs
+++ Assets/_Project/Scripts/Enemy/EnemyChaseAI.cs
@@ -1,102 +1,11 @@
 using System;
 using UnityEngine;
 
-public class EnemyChaseAI : MonoBehaviour
+public class EnemyChaseAI : EnemyBaseAI
 {
-    enum EnemyAIBehavior { Idle, Patrol, Chase, Retreat, Shoot, Attack }
-
-    private EnemyAIBehavior state = EnemyAIBehavior.Idle;
-    [HideInInspector] public GameObject player = null;
-    public float speed = 5;
-    public float attackDistance = 1;
-    public float disengageDistance = 1.3f;
-    public float attackCooldown = 0;
-    public int damage = 1;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
-    void Update()
+    override protected void Update()
     {
-        UpdateCooldowns();
-
-        switch (state)
-        {
-            case EnemyAIBehavior.Idle:
-                IdleBehavior();
-                break;
-            case EnemyAIBehavior.Chase:
-                ChaseBehavior();
-                break;
-            case EnemyAIBehavior.Attack:
-                AttackBehavior();
-                break;
-        }
-    }
-
-    private void UpdateCooldowns()
-    {
-        attackCooldown -= Time.deltaTime;
-    }
-
-    private void SwitchState(EnemyAIBehavior newState)
-    {
-        state = newState;
-    }
-
-    private void IdleBehavior()
-    {
-        if (player != null)
-        {
-            SwitchState(EnemyAIBehavior.Chase);
-        }
-    }
-
-    private void ChaseBehavior()
-    {
-        if (player == null)
-        {
-            SwitchState(EnemyAIBehavior.Idle);
-            return;
-        }
-
-        Vector3 playerDirection = (player.transform.position - transform.position).normalized;
-        transform.position += new Vector3(playerDirection.x, 0, playerDirection.z) * speed * Time.deltaTime;
-
-        if (Vector3.Distance(player.transform.position, transform.position) <= attackDistance)
-        {
-            SwitchState(EnemyAIBehavior.Attack);
-        }
-    }
-
-    private void AttackBehavior()
-    {
-        if (player == null)
-        {
-            SwitchState(EnemyAIBehavior.Idle);
-            return;
-        }
-
-        if (Vector3.Distance(player.transform.position, transform.position) > disengageDistance)
-        {
-            SwitchState(EnemyAIBehavior.Chase);
-        }
-        else
-        {
-            if (attackCooldown <= 0)
-            {
-                Health health = player.GetComponent<Health>();
-                if (health != null)
-                {
-                    health.TakeDamage(damage);
-                }
-
-                attackCooldown = 2f;
-            }
-        }
+        base.Update();
     }
 }
