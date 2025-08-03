fede08cd8e889b6da989dacdbd6360bfb9933d29
AshleyWills <41488426+AshleyWillsDev@users.noreply.github.com>
2025-08-03 19:12:01 +0100

    Added pedestal aura

--- Assets/_Project/Scripts/Portal.cs
+++ Assets/_Project/Scripts/Portal.cs
@@ -1,3 +1,4 @@
+using FMODUnity;
 using UnityEngine;
 using UnityEngine.Experimental.GlobalIllumination;
 
@@ -9,17 +10,30 @@ public class Portal : MonoBehaviour
     [SerializeField] private GameObject Spotlight;
     [SerializeField] private CapsuleCollider capsuleCollider;
     [SerializeField] private GameObject PedestalItem;
+    [SerializeField] private StudioEventEmitter openPortalEmitter;
+
+
+    private void Start()
+    {
+        if (gameLevel == 0)
+        {
+            openPortalEmitter.Play();
+        }
+        
+    }
 
     public void OpenPortal()
     {
         Spotlight.gameObject.SetActive(true);
         this.gameObject.SetActive(true);
         _isUnlocked = true;
+        openPortalEmitter.Play();
     }
 
     public void ClosePortal()
     {
         capsuleCollider.enabled = false;
+        openPortalEmitter.Stop();
     }
 
     private void OnTriggerEnter(Collider other)
