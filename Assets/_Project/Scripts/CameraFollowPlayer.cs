70196363162d90d2051f2846a481a2ff77eaf450
RiffStudios <68124263+RiffStudios@users.noreply.github.com>
2025-08-02 22:22:35 -0700

    Adding Harmony to main HUB, generating forest  + mini map

---
+++ Assets/_Project/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
